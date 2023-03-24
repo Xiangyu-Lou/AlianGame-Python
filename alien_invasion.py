f4e03b6c45942c0f8b68be50df6859448f5eb477
Alex <alex.xy.lou@gmail.com>
2023-03-24 15:13:46 +0800

    creat

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+import settings
+
+class AlienInvasion:
+    """管理游戏资源和行为的类 """
+    
+    def _init_(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init()
+        self.settings = settings.Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        pygame.display.set_caption("Alien Invasion")
+        
+    def run_game(self):
+        """开始游戏的主循环"""
+        while True:
+            """监视键盘和鼠标事件"""
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill(self.settings.bg_color)
+            pygame.display.flip()
+    
+if __name__ == '_main_':
+    ai = AlienInvasion()
+    ai.run_game()
